6add980b83bb8819960757334fc6fdd9e730fcb1
bayendemba <bayendemba@gmail.com>
2019-10-23 20:41:32 +0000

    Template login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,90 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+  <!-- Begin page -->
+        <div class="accountbg"></div>
+        <div class="home-btn d-none d-sm-block">
+                <a href="index.html" class="text-white"><i class="fas fa-home h2"></i></a>
+            </div>
+        <div class="wrapper-page">
+                <div class="card card-pages shadow-none">
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="card-body">
+                        <div class="text-center m-t-0 m-b-15">
+                                <a href="index.html" class="logo logo-admin"><img src="assets/images/logo-dark.png" alt="" height="24"></a>
                         </div>
+                        <h5 class="font-18 text-center">Agroconnect</h5>
+                        <form class="form-horizontal m-t-30" method="POST" action="{{ route('login') }}">
+                                {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                                        <div class="col-12">
+                                                <label>Username</label>
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                                        <div class="col-12">
+                                                <label for="password">Password</label>
+                                        <input id="password" type="password" class="form-control" name="password" required>
+
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <div class="col-12">
+                                        <div class="checkbox checkbox-primary">
+                                                <div class="custom-control custom-checkbox">
+                                                        <input type="checkbox" name="remember" class="custom-control-input" id="customCheck1" {{ old('remember') ? 'checked' : '' }}>
+                                                        <label class="custom-control-label" for="customCheck1">
+                                                            Remember Me
+                                                        </label>
+                                                </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group text-center m-t-20">
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-primary btn btn-primary btn-block btn-lg waves-effect waves-light">
+                                            Connexion
+                                        </button>
+                                    </div>
+
+                                    {{-- <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            Forgot Your Password?
+                                        </a> --}}
+                                    <div class="form-group row m-t-30 m-b-0">
+                                        <div class="col-sm-7">
+                                            <a href="{{ route('password.request') }}" class="text-muted"><i class="fa fa-lock m-r-5"></i> Mot de passe oublié?</a>
+                                        </div>
+                                        <div class="col-sm-5 text-right">
+                                            <a href="pages-register.html" class="text-muted">Créer un Compte</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                    </div>
+
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        <!-- END wrapper -->
+{{--  --}}
 @endsection
