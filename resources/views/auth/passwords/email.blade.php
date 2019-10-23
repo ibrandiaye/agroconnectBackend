fc97d053b776f0d9472f5d7d62a3a7bf04331ab4
bayendemba <bayendemba@gmail.com>
2019-10-23 21:10:34 +0000

    Forget password template

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,56 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
+<div class="accountbg"></div>
+    <div class="home-btn d-none d-sm-block">
+                <a href="/home" class="text-white"><i class="fas fa-home h2"></i></a>
+    </div>
+    <div class="wrapper-page">
+            <div class="card card-pages shadow-none">
+                <div class="card-body">
+                    <div class="text-center m-t-0 m-b-15">
+                            <a href="index.html" class="logo logo-admin"><img src="assets/images/logo-dark.png" alt="" height="24"></a>
+                    </div>
+
+                    <h5 class="font-18 text-center">Mot de passe oublié</h5>
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form class="form-horizontal m-t-30" method="POST" action="{{ route('password.email') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
+                                    <div class="col-12">
+                                            <label>Adresse mail</label>
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+
+                            <div class="form-group text-center m-t-20">
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary btn btn-primary btn-block btn-lg waves-effect waves-light">
+                                        Envoyer
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                </div>
+
+            </div>
+            </div>
+        <!-- END wrapper -->
+
+{{-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -43,5 +92,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
 @endsection
